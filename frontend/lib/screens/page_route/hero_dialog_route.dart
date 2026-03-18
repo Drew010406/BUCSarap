b0cc7ae4a10342a4dfa042c0dad2928b06fb086b
est_calexx <10johannesmunoz@gmail.com>
2026-03-19 00:48:54 +0800

    added: hero_dialog_route to be used instead of flutter's default navigation animation ( taken from a YT tutorial )

---
+++ frontend/lib/screens/page_route/hero_dialog_route.dart
@@ -0,0 +1,43 @@
+// https://github.com/funwithflutter/flutter_ui_tips/blob/master/tip_003_popup_card/lib/hero_dialog_route.dart
+import 'package:flutter/material.dart';
+
+
+class HeroDialogRoute<T> extends PageRoute<T> {
+  HeroDialogRoute({
+    required WidgetBuilder builder,
+    required RouteSettings super.settings,
+    super.fullscreenDialog,
+  })  : _builder = builder;
+
+  final WidgetBuilder _builder;
+
+  @override
+  bool get opaque => false;
+
+  @override
+  bool get barrierDismissible => true;
+
+  @override
+  Duration get transitionDuration => const Duration(milliseconds: 300);
+
+  @override
+  bool get maintainState => true;
+
+  @override
+  Color get barrierColor => Colors.black54;
+
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
+    return child;
+  }
+
+  @override
+  Widget buildPage(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation) {
+    return _builder(context);
+  }
+
+  @override
+  String get barrierLabel => 'Popup dialog open';
+}
