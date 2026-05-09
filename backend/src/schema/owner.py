87abc09038c18224907f9c49c2ee853e32b75829
anselmusPhytagoras32 <gebhel.anselm@gmail.com>
2026-05-10 02:01:40 +0800

    feat: rate limiter, jwt, and refresh tokens, rename the user to owner

--- backend/src/schema/owner.py
+++ backend/src/schema/owner.py
@@ -12,3 +12,7 @@ class UserResponse(BaseModel):
 
     # Translates default dictionaries to objects. This prevents app crash 
     model_config = ConfigDict(from_attributes = True)
+
+class UserLogin(BaseModel):
+    username: str = Field(min_length=3, max_length=50)
+    password: str = Field(min_length=8)
