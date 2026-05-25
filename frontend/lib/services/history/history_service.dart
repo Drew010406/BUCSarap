0567e2def7515ea0a596d5fe87ee808ffeaa7cbe
est_calexx <carlxxsinenomine@gmail.com>
2026-05-25 23:02:59 +0800

    added: TODO 5

--- frontend/lib/services/history/history_service.dart
+++ frontend/lib/services/history/history_service.dart
@@ -56,4 +56,82 @@ class HistoryService {
       }
     }
   }
+
+  Future<dynamic> getStallDailyRevenue(int stallID) async {
+    try {
+      Response response = await _dio!.get("/history/revenue/$stallID/daily");
+      if(kDebugMode) {
+        print(response.data.toString());
+      }
+      return response.data;
+    } on DioException catch (e) {
+      if (e.response != null) {
+        final statusCode = e.response?.statusCode;
+        final errorData = e.response?.data;
+
+        if (statusCode == 500) {
+          final errorMessage = errorData['detail'] ?? 'Database error';
+          throw Exception(errorMessage);
+        } else {
+          throw Exception(
+            'Server error: $statusCode = ${errorData['detail'] ?? "Unknown Error"}',
+          );
+        }
+      } else {
+        throw Exception('Network error: ${e.message}');
+      }
+    }
+  }
+
+  Future<dynamic> getStallWeeklyRevenue(int stallID) async {
+    try {
+      Response response = await _dio!.get("/history/revenue/$stallID/weekly");
+      if(kDebugMode) {
+        print(response.data.toString());
+      }
+      return response.data;
+    } on DioException catch (e) {
+      if (e.response != null) {
+        final statusCode = e.response?.statusCode;
+        final errorData = e.response?.data;
+
+        if (statusCode == 500) {
+          final errorMessage = errorData['detail'] ?? 'Database error';
+          throw Exception(errorMessage);
+        } else {
+          throw Exception(
+            'Server error: $statusCode = ${errorData['detail'] ?? "Unknown Error"}',
+          );
+        }
+      } else {
+        throw Exception('Network error: ${e.message}');
+      }
+    }
+  }
+
+  Future<dynamic> getStallMonthlyRevenue(int stallID) async {
+    try {
+      Response response = await _dio!.get("/history/revenue/$stallID/monthly");
+      if(kDebugMode) {
+        print(response.data.toString());
+      }
+      return response.data;
+    } on DioException catch (e) {
+      if (e.response != null) {
+        final statusCode = e.response?.statusCode;
+        final errorData = e.response?.data;
+
+        if (statusCode == 500) {
+          final errorMessage = errorData['detail'] ?? 'Database error';
+          throw Exception(errorMessage);
+        } else {
+          throw Exception(
+            'Server error: $statusCode = ${errorData['detail'] ?? "Unknown Error"}',
+          );
+        }
+      } else {
+        throw Exception('Network error: ${e.message}');
+      }
+    }
+  }
 }
