55159daa1987e1fbf1eaea274efdc9310a9fd8dd
Peter Andrew Bañares <peterandrew.banares24@bicol-u.edu.ph>
2026-05-28 11:01:58 +0800

    fixed: route path of getownerstallcategories kulang / HAHAHAHA

--- backend/src/routers/owner_stall.py
+++ backend/src/routers/owner_stall.py
@@ -6,11 +6,12 @@ from backend.src.db.session import get_db
 from backend.src.schema.owner_stall import *
 from backend.src.schema.stalls import CategoryInfo
 from backend.src.schema.product import ProductResponse
+from backend.src.schema.owner_stall import ProductUpdate
 
 route = APIRouter()
 
 """Wait nalang ako ng update mo para sa flow ng screen sa owner side"""
-@route.get("owners/{owner_id}/stalls/{stall_id}/categories", response_model=List[CategoryInfo])
+@route.get("/owners/{owner_id}/stalls/{stall_id}/categories", response_model=List[CategoryInfo])
 async def get_owner_stall_categories(stall_id: int, owner_id: int, db: Annotated[Connection, Depends(get_db)]):
 
     verify_query = text("""
@@ -112,6 +113,110 @@ async def accept_orders(order_id: int, stall_id: int, db: Annotated[Connection, 
         db.rollback()
         raise HTTPException(status_code=500, detail=f"Error: {str(error)}")
     
+@route.patch("/{owner_id}/products/{product_id}")
+async def update_product(product_id: int, owner_id: int, product_update: ProductUpdate, db: Annotated[Connection, Depends(get_db)]):
+    
+    verification_query = text("""
+                              
+            SELECT p.product_name, s.owner_id
+            FROM stall s
+            JOIN product_category pc ON pc.stall_id = s.stall_id
+            JOIN product p ON p.category_id = pc.category_id
+            WHERE p.product_id = :p_id AND s.owner_id = :o_id
+        """)
+    
+    results = db.execute(verification_query, {"p_id": product_id, "o_id": owner_id}).mappings().fetchone()
+    
+    if not results:
+        raise HTTPException(status_code=404, detail=f"Product with ID: {product_id} of Owner ID: {owner_id} was not found.")
+
+    update_data = product_update.model_dump(exclude_unset=True)
+    
+    if not update_data:
+        raise HTTPException(status_code=400, detail="No fields to update.")
+    
+    update_fields = []
+    params = {"product_id": product_id}
+    
+    for field,value in update_data.items():
+        update_fields.append(f"{field} = :{field}")
+        params[field] = value
+        
+    update_query = text(f"""
+           
+        UPDATE product
+        SET {','.join(update_fields)}
+        WHERE product_id = :product_id             
+        """)    
+    
+    try:
+        
+        db.execute(update_query, params)
+        db.commit()
+    
+        check_query = text("""
+                           
+            SELECT * 
+            FROM product
+            WHERE product_id = :product_id
+            """)
+        
+        updated_product = db.execute(check_query, {"product_id": product_id}).mappings().fetchone()
+        return updated_product
+    
+    except Exception as error:
+        
+        db.rollback()
+        raise HTTPException(status_code= 500, detail=f"Database error: {str(error)}")    
+
+@route.patch("/{owner_id}/products/{product_id}/toggle-status")
+async def toggle_product_status(product_id: int, owner_id:int, db: Annotated[Connection, Depends(get_db)]):
+    
+    verification_query = text("""
+                              
+            SELECT p.product_name, p.product_status, s.owner_id
+            FROM stall s
+            JOIN product_category pc ON pc.stall_id = s.stall_id
+            JOIN product p ON p.category_id = pc.category_id
+            WHERE p.product_id = :p_id AND s.owner_id = :o_id
+        """)
+    
+    results = db.execute(verification_query, {"p_id": product_id, "o_id": owner_id}).mappings().fetchone()
+    
+    if not results:
+        raise HTTPException(status_code=404, detail=f"Product with ID: {product_id} of Owner ID: {owner_id} was not found.")
+
+    product_status = results["product_status"]
+    
+    toggled_status = not product_status
+    
+    update_query = text("""
+                        
+        UPDATE product
+        SET product_status = :p_status
+        WHERE product_id = :p_id
+        """)
+    
+    try:
+        
+        db.execute(update_query, {"p_status": toggled_status, "p_id": product_id})
+        db.commit()
+        
+        check_query = text("""
+                           
+            SELECT * 
+            FROM product
+            WHERE product_id = :product_id
+            """)
+        
+        updated_product = db.execute(check_query, {"product_id": product_id}).mappings().fetchone()
+        return updated_product
+
+    except Exception as error:
+        
+        db.rollback()
+        raise HTTPException(status_code= 500, detail=f"Database error: {str(error)}")    
+
 @route.delete("/orders/{order_id}")
 async def decline_order(order_id: int, stall_id: int, db: Annotated[Connection, Depends(get_db)]):
     
@@ -125,7 +230,7 @@ async def decline_order(order_id: int, stall_id: int, db: Annotated[Connection, 
     results = db.execute(verification_query, {"o_id": order_id, "s_id": stall_id}).mappings().fetchone()
 
     if not results:
-        raise HTTPException(status_code=404, details="Order not found or Order is no longer in Pending phase.")
+        raise HTTPException(status_code=404, detail="Order not found or Order is no longer in Pending phase.")
 
     # First delete order line items, then delete the order
     delete_line_items_query = text("""
@@ -139,9 +244,8 @@ async def decline_order(order_id: int, stall_id: int, db: Annotated[Connection, 
     """)
     
     try:
-        # Delete dependent order_line items first
+
         db.execute(delete_line_items_query, {"o_id": order_id})
-        # Then delete the order
         db.execute(delete_order_query, {"o_id": order_id})
         db.commit()
         
@@ -150,4 +254,41 @@ async def decline_order(order_id: int, stall_id: int, db: Annotated[Connection, 
     except Exception as error:
         db.rollback()
         raise HTTPException(status_code=500, detail=f"Error: {str(error)}")
+
+@route.delete("/{owner_id}/products/{product_id}")
+async def delete_product(owner_id: int, product_id: int, db: Annotated[Connection, Depends(get_db)]):
+    
+    verification_query = text("""
+                              
+            SELECT p.product_name, s.owner_id
+            FROM stall s
+            JOIN product_category pc ON pc.stall_id = s.stall_id
+            JOIN product p ON p.category_id = pc.category_id
+            WHERE p.product_id = :p_id AND s.owner_id = :o_id
+        """)
+    
+    results = db.execute(verification_query, {"p_id": product_id, "o_id": owner_id}).mappings().fetchone()
+    
+    if not results:
+        raise HTTPException(status_code=404, detail=f"Product with ID: {product_id} of Owner ID: {owner_id} was not found.")
+
+    delete_query = text("""
+                        
+            DELETE p
+            FROM product p 
+            JOIN product_category pc ON pc.category_id = p.category_id
+            JOIN stall s ON s.stall_id = pc.stall_id
+            WHERE p.product_id = :p_id AND s.owner_id = :o_id                        
+        """)
+    
+    try:
+        
+        results = db.execute(delete_query, {"p_id": product_id, "o_id": owner_id })
+        db.commit()
+        return {"message": f"Successfully deleted Product {product_id} OF Owner {owner_id}"}
+    
+    except Exception as error:
+        
+        db.rollback()
+        raise HTTPException(status_code=500, detail= f"Error: {str(error)}")
     
